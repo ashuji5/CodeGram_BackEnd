61914550cf576dc23d3b47b7923e29e645088b99
Ashwani Nayak <heavenash1998@gmail.com>
2021-07-31 18:28:18 +0530

    Backend Of CodeGram

--- controllers/postController.js
+++ controllers/postController.js
@@ -89,6 +89,8 @@ const deletePost = async (req, res) => {
 
     try {
 
+      console.log();
+
         const { id } = req.params;
 
         if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
